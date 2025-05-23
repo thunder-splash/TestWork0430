1294630aafc8b226da6e39e48f0774884dbb6f1b
thunder-splash <111632585+thunder-splash@users.noreply.github.com>
2025-05-23 20:49:34 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/TestWork0430',
 };
 
 export default nextConfig;
